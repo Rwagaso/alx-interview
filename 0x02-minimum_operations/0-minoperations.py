916e920293855bff79d75fa18d9ee07ce7930db5
Rwagaso <stevenrwa31@gmail.com>
2024-07-11 02:33:42 -0700

    more

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
-"""Solution to alx interview question
-"""
-
-
-def minOperations(n):
-    """Minimum Operations
-    @param (n): <int>
-
-    Return: <int> fewest number of operation need to result in exactly n of H
+""" Minimum Operations
     """
-    if n <= 1:
-        return 0
 
-    count = 0
-    for i in range(2, n + 1):
-        while n % i == 0:
-            count += i
-            n //= i
-    return count
+
+def minOperations(n: int) -> int:
+    """ Minimum Operations needed to get n H characters """
+    next = 'H'
+    body = 'H'
+    op = 0
+    while (len(body) < n):
+        if n % len(body) == 0:
+            op += 2
+            next = body
+            body += body
+        else:
+            op += 1
+            body += next
+    if len(body) != n:
+        return 0
+    return op
